b1a53d008f53bc9eb2aa1d264c96e4f4950d5354
黄兵 <1278032416@qq.com>
2021-03-27 09:09:55 +0800

    update

--- main.go
+++ main.go
@@ -9,18 +9,23 @@ import (
 	"os"
 	"regexp"
 	"strings"
+	"time"
 )
 
 func main() {
 	govList := home()
+	govList = append(govList, "gov-.htm")
+	govAll := make([]string, 0)
 	for _, gov := range govList {
 		govs := getGovList("http://www.5566.net/" + gov)
-		fmt.Println(govs)
+		govAll = append(govAll, govs...)
 	}
+	fmt.Println(govAll)
+	fmt.Println(len(govAll))
 }
 
 func home() []string {
-	bodyStr := getBody("http://www.5566.net/gov-.htm")
+	bodyStr := tryGetBody("http://www.5566.net/gov-.htm", 3)
 	compile := regexp.MustCompile(`<a class="p12" href="(.*?)" target="_self">.*?</a>`)
 	allString := compile.FindAllStringSubmatch(bodyStr, -1)
 	govList := make([]string, len(allString))
@@ -32,21 +37,49 @@ func home() []string {
 
 //get gov.cn list
 func getGovList(url string) []string {
-	bodyStr := getBody(url)
-	compile := regexp.MustCompile(`,.*.gov.cn`)
-	allString := compile.FindAllStringSubmatch(bodyStr, -1)
-	var govStr string
-	for _, match := range allString {
-		govStr = match[0]
-	}
-	if len(govStr) != 0 {
+	bodyStr := tryGetBody(url, 3)
+	limiterCompile := regexp.MustCompile(`.wscckey=`)
+	islimiter := limiterCompile.MatchString(bodyStr)
+	if islimiter {
+		url = getLimiterUrl(bodyStr)
+		return getGovList(url)
+	} else {
+		compile := regexp.MustCompile(`,.*.gov.cn`)
+		allString := compile.FindAllStringSubmatch(bodyStr, -1)
+		var govStr string
+		for _, match := range allString {
+			govStr = match[0]
+		}
+		if len(govStr) != 0 {
 
-		return strings.Split(govStr, ",")
+			return strings.Split(govStr, ",")
+		}
+		return nil
 	}
-	return nil
+}
+func getLimiterUrl(body string) string {
+	urlReg := `<HTML><HEAD><script>window.location.href="(.*?)";</script></HEAD><BODY>`
+	limiterCompile := regexp.MustCompile(urlReg)
+	limiterUrl := limiterCompile.FindAllStringSubmatch(body, -1)
+	return limiterUrl[0][1]
 }
 
-func getBody(url string) string {
+func tryGetBody(url string, num int) string {
+	response := getBody(url)
+	if response.StatusCode != 200 && num > 0 {
+		for {
+			num--
+			fmt.Printf("[Error] %d 休眠 30 秒重试 \n", response.StatusCode)
+			time.Sleep(time.Duration(30) * time.Second)
+			return tryGetBody(url, num)
+		}
+	} else {
+		body := response.Body
+		return readBody(body)
+	}
+}
+
+func getBody(url string) *http.Response {
 	client := &http.Client{}
 	request, err := http.NewRequest("GET", url, nil)
 	if err != nil {
@@ -61,8 +94,7 @@ func getBody(url string) string {
 		fmt.Println("fatal error")
 		panic(err)
 	}
-	body := response.Body
-	return readBody(body)
+	return response
 }
 
 // 读取body
